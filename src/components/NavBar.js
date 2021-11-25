97a5f1314be870a7d73c165aaf713dedc23d0f75
eliza29 <elizaseguraf@gmail.com>
2021-11-24 19:39:59 -0500

    menu

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+
+const NavBar = () => {
+    return (
+        <>
+            <header className="w-full fixed bg-gray-50 flex justify-between p-4">
+                Tienda Online
+                <nav className="flex justify-evenly space-x-6">
+                    <a href="cat1">Categoria 1</a>
+                    <a href="cat2">Categoria 2</a>
+                    <a href="cat3">Categoria 3</a>
+                </nav>
+            </header>
+        
+        </>
+        
+    )
+}
+
+export default NavBar
+
